c474e9d373ab589dad21741782204e4001472e0f
Grigorii Matiukhin <grigory36m@gmail.com>
2023-02-27 21:17:54 +0300

    polkit gnome service working with i3wm

---
+++ services/polkit-gnome-authentication-agent-1.nix
@@ -0,0 +1,22 @@
+{config, pkgs, lib, ...}:
+
+{
+  systemd.services = {
+    polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        ExecStop = "";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  }; 
+
+  systemd.services.polkit-gnome-authentication-agent-1.enable = true;
+}
