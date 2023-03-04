17cd94308b2da1d483a21bf3312ae8ade491de44
Grigorii Matiukhin <grigory36m@gmail.com>
2023-03-04 23:06:22 +0300

    Added zfs.nix and hardware-configuration.nix files, minor i3 change

---
+++ zfs.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+
+{
+  networking.hostId = "cc73853f";
+  boot = {
+    supportedFilesystems = [ "zfs" ];
+    kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+    loader = {
+      efi = {
+        efiSysMountPoint = "/boot/efis/nvme0n1p1";
+        canTouchEfiVariables = true;
+      };
+
+      generationsDir.copyKernels = true;
+
+      grub = {
+        efiInstallAsRemovable = false;
+        enable = true;
+        version = 2;
+        copyKernels = true;
+        efiSupport = true;
+        zfsSupport = true;
+        # extraPrepareConfig = ''
+        #   mkdir -p /boot/efis
+        #   for i in  /boot/efis/*; do mount $i ; done
+        #
+        #   mkdir -p /boot/efi
+        #   mount /boot/efi
+        # '';
+        extraInstallCommands = ''
+        ESP_MIRROR=$(mktemp -d)
+        cp -r /boot/efis/nvme0n1p1/EFI $ESP_MIRROR
+        for i in /boot/efis/*; do
+         cp -r $ESP_MIRROR/EFI $i
+        done
+        rm -rf $ESP_MIRROR
+        '';
+        devices = [
+          "/dev/nvme0n1"
+        ];
+      };
+    };
+  };
+}
